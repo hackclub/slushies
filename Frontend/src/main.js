b98cc2ca926a4fcecaeb8480ce28e93c16ab75ae
MustafaBioS <mustaafahany@gmail.com>
2026-02-09 23:04:43 +0200

    ADD - FAQ Questions

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,5 +1,10 @@
 const flag = document.querySelector('.flag');
 const q = document.querySelectorAll('.q');
+const faqMod = document.querySelector('.faqMod');
+const modCon = document.querySelector('.modCon');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close');
+const faqBtn = document.querySelector('.faq')
 
 
 flag.addEventListener('click', ()=> {
@@ -10,6 +15,9 @@ q.forEach(qs => {
     qs.addEventListener('click', (toggleFAQ))
 })
 
+close.addEventListener('click', (modal));
+faqBtn.addEventListener('click', (modal));
+
 function toggleFAQ(e) {
     const current = e.currentTarget;
 
@@ -18,4 +26,14 @@ function toggleFAQ(e) {
     });
 
     current.classList.toggle('open');
+}
+
+function modal() {
+    if (modCon.style.display == "flex") {
+        modCon.style.display = 'none';
+        overlay.style.display = 'none';
+    } else {
+        modCon.style.display = 'flex';
+        overlay.style.display = 'flex';
+    }
 }
